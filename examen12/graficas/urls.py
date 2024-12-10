6da1a0a9153679f2b24da97c4c6fae45d6595b3f
Rodrigo Vasto <sabradios@live.com.mx>
2024-12-09 21:34:20 -0600

    feat: :white_check_mark: Estructura

--- examen12/graficas/urls.py
+++ examen12/graficas/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
- 
 urlpatterns = [
-    path('',views.graficas_view, name= 'grafica_view'),
+    path('', views.graficas, name='graficas'),
 ]
