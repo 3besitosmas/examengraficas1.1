eb25a6c5ac82708af5384229f1487ff1d12c5498
Rodrigo Vasto <sabradios@live.com.mx>
2024-12-09 21:35:16 -0600

    feat: :sparkles: Logica

--- examen12/graficas/views.py
+++ examen12/graficas/views.py
@@ -1,37 +1,20 @@
 from django.shortcuts import render
 
-def get_graph_data():
-    """
-    Helper function to return sample data for graphs.
-    Returns:
-        A dictionary with sample graph data.
-    """
-    return [
-        {
-            'etiquetas': ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'],
-            'datos': [5, 15, 10, 20, 25]
-        },
-        {
-            'etiquetas': ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'],
-            'datos': [5, 15, 10, 20, 25]
-        },
-        {
-            'etiquetas': ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'],
-            'datos': [268, 156, 250, 20, 25]
-        }
-    ]
-
-def graficas_view(request):
-    """
-    Render the graphs view with prepared data.
-    Args:
-        request: HTTP request object.
-    Returns:
-        Rendered HTML template with graph data.
-    """
-    graph_data = get_graph_data()
-    return render (request, 'graficas/graficas.html', {
-        'grafica1': graph_data[0],
-        'grafica2': graph_data[1],
-        'grafica3': graph_data[2],
-})
+def graficas(request):
+    # Datos para las seis gráficas
+    context = {
+        'data1': [5, 10, 15, 20],
+        'labels1': ['Costo 1', 'Costo 2', 'Costo 3', ' Costo 4'],
+        'data2': [30, 50, 65, 85],
+        'labels2': ['Japon', 'Estados Unidos', 'Mexico', 'China'],
+        'data3': [3, 5, 7, 10],
+        'labels3': ['Almacen 1', 'Almacen 2', 'Almacen 3', 'Almacen 4'],
+        'data4': [4, 12, 24, 16],
+        'labels4': ['Bimestre 1', 'Bimestre 2', 'Bimestre 3', 'Bimestre 4'],
+        'data5': [12, 14, 16, 18],
+        'labels5': ['One Piece ', 'Dragon Ball', 'Citrus', 'Naruto'],
+        'data6': [16, 24, 12, 20],
+        'labels6': ['Samsung', 'iPhone de Apple', 'Xiaomi', 'Oppo'],
+    }
+    
+    return render(request, 'graficas/graficas.html', context)
